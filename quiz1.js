f85b2c492f8646036ef3b1d3420564e0b88170df
M-Danial-Awais <danich316@gmail.com>
2022-09-24 04:28:50 -0700

    add files

---
+++ quiz1.js
@@ -0,0 +1,47 @@
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// let result;
+
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+
+
+
+
+
+var array = [12, 13, 14, 14];
+    sum = 0;
+    minus = 0;
+for (i = 0; i < array.length; i++) 
+   {
+    sum += array[i];
+    minus -= array[i];
+    }
+console.log('Sum : '+sum + ' minus :  ' +minus); 
+
+
+
+
+
+function getLog(x) {
+    return Math.log(x);
+  }
+  console.log(getLog(10));
+
+
+
